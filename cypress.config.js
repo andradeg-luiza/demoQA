04e0d3c7f8d593bc2860ca03153fcf26188c3222
Luiza Andrade <andradeg.luiza@outlook.com>
2024-11-13 15:07:11 -0300

    <chore> configure experimental session and origin settings in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
-const { defineConfig } = require('cypress')
-const cucumber = require('cypress-cucumber-preprocessor').default
+const { defineConfig } = require('cypress');
+const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://demoqa.com',
     setupNodeEvents(on, config) {
-      on('file:preprocessor', cucumber())
+      on('file:preprocessor', cucumber());
     },
     specPattern: 'cypress/e2e/Features/*.feature',
+    experimentalSessionAndOrigin: true, // Adicionado para preservar cookies e dados da sessão
+	  testIsolation: false,
   },
-})
-
+});
