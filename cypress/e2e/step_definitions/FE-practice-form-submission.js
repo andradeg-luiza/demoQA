9dd941d1acca8d935fa097d245390fb60dab157c
Luiza Andrade <andradeg.luiza@outlook.com>
2024-11-12 21:56:39 -0300

    <feat> imported the locators file into the steps

--- cypress/e2e/step_definitions/FE-practice-form-submission.js
+++ cypress/e2e/step_definitions/FE-practice-form-submission.js
@@ -1,1 +1,2 @@
-import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps'
+import { locators } from '../locators/locators'
