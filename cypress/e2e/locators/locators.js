7bbf98df8f19646bb8536b4295967cb8b2aede37
Luiza Andrade <andradeg.luiza@outlook.com>
2024-11-13 00:12:51 -0300

    <feat> add locators for forms, browser windows, progress bar, and sortable pages

--- cypress/e2e/locators/locators.js
+++ cypress/e2e/locators/locators.js
@@ -1,43 +1,69 @@
 export const locators = {
-	homePage: {
-		formsField: () => cy.get('.card-body').contains('Forms'),
-	},
+  homePage: {
+    formsField: () => cy.get('.card-body').contains('Forms'),
+  },
 
-	formsMenu: {
+  formsMenu: {
     titleForm: () => cy.get('h1').contains('Practice Form'),
     formsList: () => cy.get('.header-text').contains('Forms'),
-		practiceForm: () => cy.get('.text').contains('Practice Form'),
-		firstName: '#firstName',
-		lastName: '#lastName',
-		userEmail: '#userEmail',
-		genderFemale: '#gender-radio-1',
-		genderMale: '#gender-radio-2',
-		genderOther: '#gender-radio-3',
-    phoneNumber: '#userNumber', //aceita 10 caracteres
-    dateBirth: '#dateOfBirthInput', //a data tem que ser padrão mm/dd/aaaa
-    subjectsArea: '#subjectsContainer',
-    hobbiesSports: '#hobbies-checkbox-1',
-		hobbiesReading: '#hobbies-checkbox-2',
-		hobbiesMusic: '#hobbies-checkbox-3',
-    buttonUpload: '#uploadPicture',
-    currentAdrdress: '#currentAddress',
-    selectState: '#react-select-3-input',
-    selectCity: '#react-select-4-input',
-    submitButton: '#submit'
-	},
+    practiceFormLink: () => cy.get('.text').contains('Practice Form'),
+    firstName: () => cy.get('#firstName'),
+    lastName: () => cy.get('#lastName'),
+    userEmail: () => cy.get('#userEmail'),
+    genderFemale: () => cy.get('#gender-radio-1'),
+    genderMale: () => cy.get('#gender-radio-2'),
+    genderOther: () => cy.get('#gender-radio-3'),
+    phoneNumber: () => cy.get('#userNumber'),
+    dateBirth: () => cy.get('#dateOfBirthInput'),
+    subjectsArea: () => cy.get('#subjectsContainer'),
+    hobbiesSports: () => cy.get('#hobbies-checkbox-1'),
+    hobbiesReading: () => cy.get('#hobbies-checkbox-2'),
+    hobbiesMusic: () => cy.get('#hobbies-checkbox-3'),
+    buttonUpload: () => cy.get('#uploadPicture'),
+    currentAddress: () => cy.get('#currentAddress'),
+    selectState: () => cy.get('#react-select-3-input'),
+    selectCity: () => cy.get('#react-select-4-input'),
+    submitButton: () => cy.get('#submit'),
+  },
 
-  alertsWindowsMenu: {
-    titleAlertWindows: () => cy.get('h1').contains('Browser Windows'),
-    alerWindowstList: () => cy.get('.header-text').contains('Alerts, Frame & Windows'),
-    browserWindows: () => cy.get('.text').contains('Browser Windows'),
-    windowButton: '#windowButton' //TODO validações de texto e fechar no steps
-	},
+  browserWindowsPage: {
+    titleBrowserWindows: () => cy.get('h1').contains('Browser Windows'),
+    browserWindowsForm: () => cy.get('.text').contains('Browser Windows'),
+    newTabButton: () => cy.get('#tabButton'),
+    newWindowButton: () => cy.get('#windowButton'),
+    newMessageWindowButton: () => cy.get('#messageWindowButton'),
+  },
 
   elementsMenu: {
-    titleAlertWindows: () => cy.get('h1').contains('Web Tables'),
-    alerWindowstList: () => cy.get('.header-text').contains('Elements'),
-    browserWindows: () => cy.get('.text').contains('Web Tables'),
-    //TODO Finalizar elementos
-	}
+    titleWebTables: () => cy.get('h1').contains('Web Tables'),
+    elementsList: () => cy.get('.header-text').contains('Elements'),
+    webTablesForm: () => cy.get('.text').contains('Web Tables'),
+    addButton: () => cy.get('#addNewRecordButton'),
+    editButton: () => cy.get('.action-buttons[title="Edit"]'),
+    deleteButton: () => cy.get('[data-toggle="tooltip"][title="Delete"]'),
+    firstName: () => cy.get('#firstName'),
+    lastName: () => cy.get('#lastName'),
+    age: () => cy.get('#age'),
+    email: () => cy.get('#userEmail'),
+    salary: () => cy.get('#salary'),
+    department: () => cy.get('#department'),
+    submitButton: () => cy.get('#submit'),
+    rows: () => cy.get('.rt-tbody .rt-tr-group'),
+  },
 
+  progressBarPage: {
+    titleProgressBar: () => cy.get('h1').contains('Progress Bar'),
+    progressBarForm: () => cy.get('.text').contains('Progress Bar'),
+    progressBar: () => cy.get('#progressBar'),
+    startButton: () => cy.get('#startButton'),
+    stopButton: () => cy.get('#stopButton'),
+    resetButton: () => cy.get('#resetButton'),
+    progressValue: () => cy.get('#progressBar').invoke('attr', 'aria-valuenow'),
+  },
+
+  sortablePage: {
+    titleSortable: () => cy.get('h1').contains('Sortable'),
+    sortableList: () => cy.get('#sortable'),
+    sortableItems: () => cy.get('#sortable li'),
+  },
 };
