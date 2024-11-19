ecdc537ba9b2f432599f5ff627b792b448b86e60
Luiza Andrade <andradeg.luiza@outlook.com>
2024-11-19 10:19:14 -0300

    <fix> Update the cypress.config.js file in the specPattern property because the feature files were not being located.

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ const cucumber = require("cypress-cucumber-preprocessor").default
 module.exports = defineConfig({
   e2e: {
     defaultCommandTimeout: 1000,
-    specPattern: "**/*/.feature",
+    specPattern: "cypress/e2e/feature/*.feature",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber());
     },
