0dbffe5267245473dca38ef696280aef602ad2c1
Luiza Andrade <andradeg.luiza@outlook.com>
2024-11-19 00:02:16 -0300

    <feat> setting up cypress-preprocessor

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("Cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 1000,
+    specPattern: "**/*/.feature",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("file:preprocessor", cucumber());
     },
   },
 });
